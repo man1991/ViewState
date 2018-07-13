9b2af9d4e21b9e081044f103e2d38d2403e4f9ff
man1991 <man.222.sin@gmail.com>
2018-07-13 12:08:25 +0530

    #1: Added fully functional code.

---
+++ ViewState/DemoViewState.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DemoViewState.aspx.cs" Inherits="ViewState.DemoViewState" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        User Name :- <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Password :-  <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Restore" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ViewState/DemoViewState.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewState
+{
+    public partial class DemoViewState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Declaration of a and b 
+        //public string a, b;
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Value of Textbox1 and TectBox2 is assigin on the ViewState  
+            ViewState["name"] = TextBox1.Text;
+            ViewState["password"] = TextBox2.Text;
+            ////TextBox1 and TextBox2 Value is Assigning on the variable a and b  
+            //a = TextBox1.Text;
+            //b = TextBox2.Text;
+            ////after clicking on Button TextBox value Will be Cleared  
+            TextBox1.Text = TextBox2.Text = String.Empty;
+
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            //If ViewState Value is not Null then Value of View State is Assign to TextBox 
+            if (ViewState["name"] != null)
+            {
+                TextBox1.Text = ViewState["name"].ToString();
+            }
+            if (ViewState["password"] != null)
+            {
+                TextBox2.Text = ViewState["password"].ToString();
+            }
+            //value of variable a and b is assingning on TextBox1 and Textbox2 
+            //TextBox1.Text = a;
+            //TextBox2.Text = b;
+        }
+    }
+}
